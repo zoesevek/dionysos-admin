6f9914225ead6dbdb10ab8995d050c21ad7bbd21
zoesevek <zoadamp@gmail.com>
2018-03-29 22:21:56 +0300

    mikri allagi

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
     def hello
-       render html: "OPA OPA" 
+       render html: "ena DIPO" 
     end
 end
